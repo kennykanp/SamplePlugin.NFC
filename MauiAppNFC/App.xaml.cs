7e9eca24cee60d16234b31017cf3c097e793e466
kenny.nino <knino@t-idsolutions.com>
2024-02-02 09:44:07 -0500

    SUBIR NFC

---
+++ MauiAppNFC/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppNFC
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppNFC/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppNFC
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
